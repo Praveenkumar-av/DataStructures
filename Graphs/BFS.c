c6ad48d521e9118b8602f43afcc9f0895db13934
Praveenkumar-av <109723900+Praveenkumar-av@users.noreply.github.com>
2023-11-30 23:13:41 +0530

    BFS and DFS added

--- Graphs/BFS.c
+++ Graphs/BFS.c
@@ -1,62 +1,59 @@
-// Breadth first search in graphs
+// Breadth first search implementation for graphs
 
 #include<stdio.h>
 #include<stdlib.h>
 
-void bfs(int **,int *,int);
 void enqueue(int *,int);
 int dequeue(int *);
 int isEmpty();
 
 int front=-1, rear=-1;
 
+void bfs(int **adj,int *q,int n,int start)
+{
+    int v, u = start;
+    int *visited = (int *)calloc(n,sizeof(int));
+
+    printf("%d ",u);  // start node
+    enqueue(q,u);
+    while(!isEmpty())
+    {
+        u = dequeue(q);
+
+        for(v=1;v<n;v++)
+        {
+            if(adj[u][v] == 1 && visited[v] == 0)
+            {   
+                printf("%d \n",v);
+                visited[v] = 1;
+                enqueue(q,v);
+            }
+        }
+    }
+}
+
 void main()
 {
-    int n, i, j;
+    int n, i, j, start;
 
     printf("Enter the no. of vertices :");
     scanf("%d",&n);
 
+    // create the adjacency matrix dynamically
     int **adj = (int **)calloc(n,sizeof(int *));
-    int *q = (int *)calloc(n,sizeof(int));
-
     for(i=0;i<n;i++)
         adj[i] = (int *)calloc(n,sizeof(int));
+
+    // create the queue dynamically
+    int *q = (int *)calloc(n,sizeof(int));
 
     for(i=0;i<n;i++)
         for(j=0;j<n;j++)
             scanf("%d",&adj[i][j]);
 
-    bfs(adj,q,n);
-}
-
-void bfs(int **adj,int *q,int n)
-{
-    int i,j, e;
-    int *visited = (int *)calloc(n,sizeof(int));
-
     printf("Enter the root :");
-    scanf("%d",&e);
-
-    enqueue(q,e);
-    while(!isEmpty())
-    {
-        e = dequeue(q);
-
-        if(visited[e] != 1)
-        {
-            printf("%d ",e);
-            visited[e] = 1;
-
-            for(j=0;j<n;j++)
-            {
-                if(adj[e][j] == 1)
-                {
-                    enqueue(q,j);
-                }
-            }
-        }
-    }
+    scanf("%d",&start);
+    bfs(adj,q,n,start);
 }
 
 void enqueue(int *q,int ch)
---
+++ Graphs/DFS.c
@@ -0,0 +1,43 @@
+// Depth first search implementation using recursion for graphs
+
+#include<stdio.h>
+#include<stdlib.h>
+
+void dfs(int **adj,int n,int *visited,int s)
+{
+    if(visited[s] == 0){
+    printf("%d ",s);
+    visited[s] = 1;
+    for(int i=0;i<n;i++)
+    {
+        if(adj[s][i] == 1 && visited[i] == 0)
+        {
+            dfs(adj,n,visited,i);
+        }
+    }
+    }
+}
+
+void main()
+{
+    int n, i, j, start;
+
+    printf("Enter the no. of vertices :");
+    scanf("%d",&n);
+
+    // create matrix dynamically
+    int **adj = (int **)calloc(n,sizeof(int *));
+    for(i=0;i<n;i++)
+        adj[i] = (int *)calloc(n,sizeof(int));
+
+    // create the visited array dynamically
+    int *visited = (int *)calloc(n,sizeof(int));
+
+    for(i=0;i<n;i++)
+        for(j=0;j<n;j++)
+            scanf("%d",&adj[i][j]);
+
+    printf("Enter the root :");
+    scanf("%d",&start);
+    dfs(adj,n,visited,start);
+}
