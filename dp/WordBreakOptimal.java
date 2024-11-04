a71c7ba6e22bccf1352cde01fc9e83dbc74a73f0
Praveenkumar-av <109723900+Praveenkumar-av@users.noreply.github.com>
2024-11-04 14:27:55 +0530

    combination sum added

--- dp/WordBreakOptimal.java
+++ dp/WordBreakOptimal.java
@@ -3,6 +3,22 @@ import java.util.*;
 
 class WordBreakOptimal {
 
+    static int wordBreakTab(int n, String s, ArrayList<String> dictionary) {
+        int len = s.length();
+        int[][] dp = new int[n][len+1];
+
+        dp[0][0] = 1;
+        
+        for(int ind=1; ind<=len; ind++) {
+            for (int i = ind; i > 0; i--) {
+                if (dp[ind][i-1] > 0 && dictionary.contains(s.substring(i-1, ind))) 
+                        dp[ind][i] = 1;
+            }
+        }
+                
+        return dp[n][len];
+    }
+
     static int wordBreak(int n, String s, ArrayList<String> dictionary) {
         int len = s.length();
         int[] dp = new int[len+1];
@@ -18,7 +34,7 @@ class WordBreakOptimal {
     }
 
     public static void main(String[] args) {
-        String s = "ilikesam";
+        String s = "ilike";
 
         ArrayList<String> dictionary = new ArrayList<>(List.of("i", "like", "sam", "sung", "samsung", "mobile"));
 
